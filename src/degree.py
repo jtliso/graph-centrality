9a2fb5115848a3dcfcc763625ff67eec32911436
J.T. Liso <jtliso417@gmail.com>
2018-04-03 08:52:31 -0400

    added code for plotting times

--- src/degree.py
+++ src/degree.py
@@ -2,6 +2,9 @@
 import networkx as nx
 import sys
 import operator
+import matplotlib.pyplot as plt
+import time
+import numpy as np
 
 def degree_centrality(G):
     centrality = {}
@@ -12,17 +15,30 @@ def degree_centrality(G):
 
     return centrality
 
-# open and read in graph
-fname = sys.argv[1]
-f = open(fname, 'r')
-lines = f.readlines()
-f.close()
+# creates a graph of tab delimited edges listed line by line
+def create_graph(fname):
+    # open and read in graph
+    fname = sys.argv[1]
+    f = open(fname, 'r')
+    lines = f.readlines()
+    f.close()
 
-G = nx.Graph()
+    G = nx.Graph()
 
-# create Graph
-for line in lines[1:]:
-    a, b = line.strip("\n").split("\t")
-    G.add_edge(a, b)
+    # create Graph
+    for line in lines[1:]:
+        a, b = line.strip("\n").split("\t")
+        G.add_edge(a, b)
+    return G
 
-print degree_centrality(G)
+times = []
+
+for i in range(5, 10005, 10):
+    G =  nx.gnm_random_graph(i, 2*i)
+    print i
+    start_time = time.time()
+    degree_centrality(G)
+    times.append(time.time() - start_time)
+
+plt.scatter(np.arange(5, 10005, 10), times)
+plt.show()
